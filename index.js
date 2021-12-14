28f798d25d2bb516dfd3cef7c6eed58f1d8f0dbd
Dan Baechtold <latebloomertech@gmail.com>
2021-12-13 20:48:06 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(name, event) {
+    let messages = [];
+    for(let i = 0; i < name.length; i++) {
+    messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(newNumber) {
+    while(newNumber >= 0); {
+        console.log(newNumber);
+    newNumber--;
+    }
+}
